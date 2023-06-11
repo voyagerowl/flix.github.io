7a28a98cd0fef5fc47543bb4a772609cbe7223c9
Voyager Owl <voyagerowl@tutanota.com>
2023-06-11 10:55:49 +0530

    created the website

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,271 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const COLORTERM: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const DESKTOP_SESSION: string;
+	export const DISPLAY: string;
+	export const GDMSESSION: string;
+	export const GNOME_DESKTOP_SESSION_ID: string;
+	export const GNOME_SETUP_DISPLAY: string;
+	export const GNOME_SHELL_SESSION_MODE: string;
+	export const GNOME_TERMINAL_SCREEN: string;
+	export const GNOME_TERMINAL_SERVICE: string;
+	export const GTK_MODULES: string;
+	export const HOME: string;
+	export const IM_CONFIG_PHASE: string;
+	export const INIT_CWD: string;
+	export const LANG: string;
+	export const LANGUAGE: string;
+	export const LESSCLOSE: string;
+	export const LESSOPEN: string;
+	export const LOGNAME: string;
+	export const LS_COLORS: string;
+	export const NODE: string;
+	export const NODE_PATH: string;
+	export const OLDPWD: string;
+	export const PATH: string;
+	export const PNPM_HOME: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const PWD: string;
+	export const QT_ACCESSIBILITY: string;
+	export const QT_IM_MODULE: string;
+	export const SESSION_MANAGER: string;
+	export const SHELL: string;
+	export const SHLVL: string;
+	export const SNAP: string;
+	export const SNAP_ARCH: string;
+	export const SNAP_COMMON: string;
+	export const SNAP_CONTEXT: string;
+	export const SNAP_COOKIE: string;
+	export const SNAP_DATA: string;
+	export const SNAP_INSTANCE_KEY: string;
+	export const SNAP_INSTANCE_NAME: string;
+	export const SNAP_LIBRARY_PATH: string;
+	export const SNAP_NAME: string;
+	export const SNAP_REAL_HOME: string;
+	export const SNAP_REEXEC: string;
+	export const SNAP_REVISION: string;
+	export const SNAP_USER_COMMON: string;
+	export const SNAP_USER_DATA: string;
+	export const SNAP_VERSION: string;
+	export const SSH_AGENT_LAUNCHER: string;
+	export const SSH_AUTH_SOCK: string;
+	export const SYSTEMD_EXEC_PID: string;
+	export const TERM: string;
+	export const USER: string;
+	export const USERNAME: string;
+	export const VTE_VERSION: string;
+	export const WAYLAND_DISPLAY: string;
+	export const XAUTHORITY: string;
+	export const XDG_CONFIG_DIRS: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const XDG_DATA_DIRS: string;
+	export const XDG_MENU_PREFIX: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const XDG_SESSION_CLASS: string;
+	export const XDG_SESSION_DESKTOP: string;
+	export const XDG_SESSION_TYPE: string;
+	export const XMODIFIERS: string;
+	export const _: string;
+	export const npm_command: string;
+	export const npm_config_node_gyp: string;
+	export const npm_config_prefix: string;
+	export const npm_config_registry: string;
+	export const npm_config_user_agent: string;
+	export const npm_execpath: string;
+	export const npm_lifecycle_event: string;
+	export const npm_lifecycle_script: string;
+	export const npm_node_execpath: string;
+	export const npm_package_dependencies_svelte_icons: string;
+	export const npm_package_devDependencies__sveltejs_adapter_auto: string;
+	export const npm_package_devDependencies__sveltejs_kit: string;
+	export const npm_package_devDependencies_autoprefixer: string;
+	export const npm_package_devDependencies_daisyui: string;
+	export const npm_package_devDependencies_postcss: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_devDependencies_svelte_check: string;
+	export const npm_package_devDependencies_tailwindcss: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const npm_package_devDependencies_vite: string;
+	export const npm_package_name: string;
+	export const npm_package_private: string;
+	export const npm_package_scripts_build: string;
+	export const npm_package_scripts_check: string;
+	export const npm_package_scripts_check_watch: string;
+	export const npm_package_scripts_dev: string;
+	export const npm_package_scripts_preview: string;
+	export const npm_package_type: string;
+	export const npm_package_version: string;
+	export const NODE_ENV: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		COLORTERM: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		DESKTOP_SESSION: string;
+		DISPLAY: string;
+		GDMSESSION: string;
+		GNOME_DESKTOP_SESSION_ID: string;
+		GNOME_SETUP_DISPLAY: string;
+		GNOME_SHELL_SESSION_MODE: string;
+		GNOME_TERMINAL_SCREEN: string;
+		GNOME_TERMINAL_SERVICE: string;
+		GTK_MODULES: string;
+		HOME: string;
+		IM_CONFIG_PHASE: string;
+		INIT_CWD: string;
+		LANG: string;
+		LANGUAGE: string;
+		LESSCLOSE: string;
+		LESSOPEN: string;
+		LOGNAME: string;
+		LS_COLORS: string;
+		NODE: string;
+		NODE_PATH: string;
+		OLDPWD: string;
+		PATH: string;
+		PNPM_HOME: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		PWD: string;
+		QT_ACCESSIBILITY: string;
+		QT_IM_MODULE: string;
+		SESSION_MANAGER: string;
+		SHELL: string;
+		SHLVL: string;
+		SNAP: string;
+		SNAP_ARCH: string;
+		SNAP_COMMON: string;
+		SNAP_CONTEXT: string;
+		SNAP_COOKIE: string;
+		SNAP_DATA: string;
+		SNAP_INSTANCE_KEY: string;
+		SNAP_INSTANCE_NAME: string;
+		SNAP_LIBRARY_PATH: string;
+		SNAP_NAME: string;
+		SNAP_REAL_HOME: string;
+		SNAP_REEXEC: string;
+		SNAP_REVISION: string;
+		SNAP_USER_COMMON: string;
+		SNAP_USER_DATA: string;
+		SNAP_VERSION: string;
+		SSH_AGENT_LAUNCHER: string;
+		SSH_AUTH_SOCK: string;
+		SYSTEMD_EXEC_PID: string;
+		TERM: string;
+		USER: string;
+		USERNAME: string;
+		VTE_VERSION: string;
+		WAYLAND_DISPLAY: string;
+		XAUTHORITY: string;
+		XDG_CONFIG_DIRS: string;
+		XDG_CURRENT_DESKTOP: string;
+		XDG_DATA_DIRS: string;
+		XDG_MENU_PREFIX: string;
+		XDG_RUNTIME_DIR: string;
+		XDG_SESSION_CLASS: string;
+		XDG_SESSION_DESKTOP: string;
+		XDG_SESSION_TYPE: string;
+		XMODIFIERS: string;
+		_: string;
+		npm_command: string;
+		npm_config_node_gyp: string;
+		npm_config_prefix: string;
+		npm_config_registry: string;
+		npm_config_user_agent: string;
+		npm_execpath: string;
+		npm_lifecycle_event: string;
+		npm_lifecycle_script: string;
+		npm_node_execpath: string;
+		npm_package_dependencies_svelte_icons: string;
+		npm_package_devDependencies__sveltejs_adapter_auto: string;
+		npm_package_devDependencies__sveltejs_kit: string;
+		npm_package_devDependencies_autoprefixer: string;
+		npm_package_devDependencies_daisyui: string;
+		npm_package_devDependencies_postcss: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_devDependencies_svelte_check: string;
+		npm_package_devDependencies_tailwindcss: string;
+		npm_package_devDependencies_typescript: string;
+		npm_package_devDependencies_vite: string;
+		npm_package_name: string;
+		npm_package_private: string;
+		npm_package_scripts_build: string;
+		npm_package_scripts_check: string;
+		npm_package_scripts_check_watch: string;
+		npm_package_scripts_dev: string;
+		npm_package_scripts_preview: string;
+		npm_package_type: string;
+		npm_package_version: string;
+		NODE_ENV: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: string]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="mockup-code">
+    <pre data-prefix="#"><code>sudo apt remove FeverFlix</code></pre> 
+    <pre data-prefix=">" class="text-warning">FeverFlix removed from computer, but not from heart.<code></code></pre> 
+    <pre data-prefix="#"><code>sudo apt install FeverFlix</code></pre> 
+    <pre data-prefix=">" class="text-success"><code>FeverFlix has been installed on the computer, now, heart needs to be fulfilled</code></pre>
+    <pre data-prefix="$" class="text-warning">exit<code></code></pre> 
+
+  </div>
+  <br />
+  <br />
+  <br />
+<center>
+    <a href="/download"><button class="btn btn-wide btn-success">DOWNLOAD</button></a>
+    <p class="mb-5">Right now app is <strong>NOT</strong> availale for iOS.</p>
+
+</center>
